b73fc4babc6d7c932f243a189011b3213abb7648
Samuel Gebrehiwet <samuel.asmelash2017@gmail.comgit config --global user.name samuel>
2023-09-05 17:30:58 +0000

    Implement login and logout functionality

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser"); // Import cookie-parser
 
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // Use cookie-parser middleware
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,15 +30,16 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"], // Pass username from cookie
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
-
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
@@ -43,28 +48,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.use(express.urlencoded({ extended: true }));
-
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
-
-
-//app.get("/u/:id", (req, res) => {
-  // const longURL = ...
-  //res.redirect(longURL);
-//});
-
-
-app.post("/urls", (req, res) => {
-  const submittedURL = req.body.longURL; // Get the submitted URL from the request body
-  console.log("Submitted URL:", submittedURL); // Log the submitted URL
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
-
-// Express.js server code
 
 // GET route for displaying the edit page
 app.get("/urls/:id/edit", (req, res) => {
@@ -73,10 +60,15 @@ app.get("/urls/:id/edit", (req, res) => {
   const templateVars = { id, longURL };
   res.render("urls_edit", templateVars);
 });
-  
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
 });
 
+// Implement the /logout endpoint
+app.post("/logout", (req, res) => {
+  res.clearCookie("username"); // Clear the username cookie
+  res.redirect("/urls"); // Redirect back to the /urls page
+});
